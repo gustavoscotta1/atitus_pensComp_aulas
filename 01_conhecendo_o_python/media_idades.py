5dac89fa8f6f5d62ff9e395a17f26b426c9fa970
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-03-12 22:34:34 -0300

    .

--- 01_conhecendo_o_python/media_idades.py
+++ 01_conhecendo_o_python/media_idades.py
@@ -1,6 +1,8 @@
 idade_A = 19
 idade_B = 19
 idade_C = 21
-media = (idade_B + idade_A + idade_C) / 3
+idade_D = 30
+idade_E = 25
+media = (idade_B + idade_A + idade_C + idade_D + idade_E) / 5
 
 print("A média é", media)
