12a4a3db29ba80df8a6b550e15cafccbc096aa88
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-03-19 19:29:26 -0300

    novas alterações

--- 02_Funcoes_e_TDD/converte_horas.py
+++ 02_Funcoes_e_TDD/converte_horas.py
@@ -13,22 +13,22 @@ def hora_para_segundo(valor):
 def dia_para_segundo(valor):
     return hora_para_segundo(valor * 24)  
 
+def test()
+    assert hora_para_minuto(0) == 0
+    assert hora_para_minuto(1) == 60
+    assert hora_para_minuto(2) == 120
 
-assert hora_para_minuto(0) == 0
-assert hora_para_minuto(1) == 60
-assert hora_para_minuto(2) == 120
+    assert minuto_para_segundo(0) == 0
+    assert minuto_para_segundo(1) == 60
+    assert minuto_para_segundo(2) == 120
 
-assert minuto_para_segundo(0) == 0
-assert minuto_para_segundo(1) == 60
-assert minuto_para_segundo(2) == 120
+    assert hora_para_segundo(0) == 0
+    assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
+    assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
 
-assert hora_para_segundo(0) == 0
-assert hora_para_segundo(1) == 3600  # 1 * 60 * 60
-assert hora_para_segundo(2) == 7200  # 2 * 60 * 60
-
-assert dia_para_segundo(0) == 0 
-assert dia_para_segundo(1) == 86400
-assert dia_para_segundo(2) == 172800
+    assert dia_para_segundo(0) == 0 
+    assert dia_para_segundo(1) == 86400
+    assert dia_para_segundo(2) == 172800
 
 
 print("Terminou com sucesso!")   
--- 02_Funcoes_e_TDD/converte_metros.py
+++ 02_Funcoes_e_TDD/converte_metros.py
@@ -5,12 +5,13 @@ def metro_para_centimetro(valor):
 def centimetro_para_metro(valor):
     return valor / 100
 
-assert metro_para_centimetro(1) == 100
-assert metro_para_centimetro(10) == 1000
-assert metro_para_centimetro(0) == 0
+def test()
+    assert metro_para_centimetro(1) == 100
+    assert metro_para_centimetro(10) == 1000
+    assert metro_para_centimetro(0) == 0
 
-assert centimetro_para_metro(100) == 1
-assert centimetro_para_metro(1000) == 10
-assert centimetro_para_metro(0) == 0
+    assert centimetro_para_metro(100) == 1
+    assert centimetro_para_metro(1000) == 10
+    assert centimetro_para_metro(0) == 0
 
 print("Terminou com sucesso!")
