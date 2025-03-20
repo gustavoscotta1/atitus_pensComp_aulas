bdee01643e7c2fc2d16629a3998c0fc2a9a989e4
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-03-19 22:05:38 -0300

    novas alterações

--- 03_variaveis_e_operadores_matematicos/pitagoras.py
+++ 03_variaveis_e_operadores_matematicos/pitagoras.py
@@ -1,26 +1,35 @@
 def area(largura, altura):
-    # Preencher
-    pass
+    return largura * altura
 
 
 def perimetro(largura, altura):
-    # Preencher
-    pass
+    return (largura + altura) * (2)
 
 
 def hipotenusa(largura, altura):
-    # Preencher
-    pass
+    return (largura**2 + altura**2)**(1/2) 
 
+def test():
+    assert area(5, 5) == 25
+    assert area(0, 5) == 0
+    assert area(10, 5) == 50
 
-assert area(5, 5) == 25
-assert area(0, 5) == 0
-assert area(10, 5) == 50
+    assert perimetro(2, 3) == 10
+    assert perimetro(5, 5) == 20
+    assert perimetro(10, 5) == 30
 
-assert perimetro(2, 3) == 10
-assert perimetro(5, 5) == 20
-assert perimetro(10, 5) == 30
+    assert hipotenusa(4, 3) == 5
+    assert hipotenusa(6, 8) == 10
+    assert hipotenusa(5, 12) == 13
 
-assert hipotenusa(4, 3) == 5
-assert hipotenusa(6, 8) == 10
-assert hipotenusa(5, 12) == 13
+print(area(5, 8))
+print(area(4, 3))
+print(area(7, 2))
+
+print(perimetro(2, 3))
+print(perimetro(5, 6))
+print(perimetro(4, 9))
+
+print(hipotenusa(4, 3))
+print(hipotenusa(6, 8))
+print(hipotenusa(5, 12))
--- 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
+++ 03_variaveis_e_operadores_matematicos/raiz_quadrada.py
@@ -1,8 +1,7 @@
 def raiz_quadrada(valor):
-    # Preencher
-    pass
+    return valor **(1/2)
 
-
-assert raiz_quadrada(9) == 3
-assert raiz_quadrada(16) == 4
-assert raiz_quadrada(25) == 5
+def test():
+    assert raiz_quadrada(9) == 3
+    assert raiz_quadrada(16) == 4
+    assert raiz_quadrada(25) == 5
