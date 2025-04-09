de986c0dd2fb9ecb53406e9ee28d04b58e0670de
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-04-09 20:55:31 -0300

    alteração

--- 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/baskhara.py
@@ -14,8 +14,8 @@ def baskhara(a, b, c):
 def test():
     assert baskhara(1, -3, 2) == [2, 1]
     assert baskhara(2, 3, -2) == [0.5, -2]
-    assert baskhara(1, -5, 6) == [2, 3]
-    assert baskhara(1, -7, 10) == [2, 5]
+    assert baskhara(1, -5, 6) == [3, 2]
+    assert baskhara(1, -7, 10) == [5, 2]
 
     assert baskhara(1, 2, 3) is None
     assert baskhara(1, 0, 0) == 0
