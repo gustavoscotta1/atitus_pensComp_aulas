82d124816afd097e3d239001ca25a275ecca33c1
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-12 14:39:12 -0300

    ...

--- 09_Dicionários/cep.py
+++ 09_Dicionários/cep.py
@@ -1,27 +1,69 @@
-def obtem_dados_endereco(cep):
-    import http.client
-    import json
+import http.client
+import json
 
-    url = f"/ws/{cep}/json/"
+LEN_CEP = 8
+
+
+def get_cep_data_from_api(val):
+    url = f"/ws/{val}/json/"
     conn = http.client.HTTPSConnection("viacep.com.br")
     conn.request("GET", url)
     response = json.loads(conn.getresponse().read().decode())
     conn.close()
-    # Exemplo de resposta
-    # {
-    #   'cep': '91110-000',
-    #   'logradouro': 'Avenida Assis Brasil',
-    #   'complemento': 'de 4000 a 6298 - lado par',
-    #   'unidade': '',
-    #   'bairro': 'São Sebastião',
-    #   'localidade': 'Porto Alegre',
-    #   'uf': 'RS',
-    #   'ibge': '4314902',
-    #   'gia': '',
-    #   'ddd': '51',
-    #   'siafi': '8801'
-    # }
     return response
+
+
+def is_cep_valid(val):
+    if len(val) != LEN_CEP:
+        return False
+    for char in val:
+        if not char.isdigit():
+            return False
+    return True
+
+
+def consuta_ceps_conhecidos(ceps_conhecidos, cep):
+    for uf in ceps_conhecidos.keys():
+        for localidade in ceps_conhecidos[uf].keys():
+            if cep in ceps_conhecidos[uf][localidade]:
+                return True
+    return False
+
+
+def main():
+    contador_consultas = 0
+    enderecos = {}
+
+    while True:
+        print("Você consultou um total de:", contador_consultas, "vezes")
+        print("Dados em memória: ", enderecos)
+        print()
+
+        cep = input("Digite o seu cep:")
+        if not is_cep_valid(cep):
+            print("CEP inválido")
+            continue
+
+        if consuta_ceps_conhecidos(enderecos, cep):
+            print("Já conhecemos este cep")
+            continue
+
+        contador_consultas += 1
+        response = get_cep_data_from_api(cep)
+
+        uf = response["uf"]
+        localidade = response["localidade"]
+
+        if uf in enderecos:
+            if localidade in enderecos[uf]:
+                enderecos[uf][localidade].append(cep)
+            else:
+                enderecos[uf][localidade] = [cep, ]
+        else:
+            enderecos[uf] = {localidade: [cep, ]}
+
+
+main()
 
 
 def validador_cep(cep):
