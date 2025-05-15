bf7ad1e0baa07f7b08ad73bae294621f52fae084
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-14 22:23:21 -0300

    alterações

--- 11_Funcoes_e_parametros_avancados/lista_primos.py
+++ 11_Funcoes_e_parametros_avancados/lista_primos.py
@@ -1,7 +1,24 @@
-def lista_primos(num):
-    pass
+def eh_primo(numero: int) -> bool:
+    if numero <= 1:
+        return False
+    i = 2
+    while numero % i != 0:
+        i = i + 1
+    if i == numero:
+        return True
+    return False
+
+def lista_primos(numero: int) -> list:
+    lista_primos = []
+    for j in range(numero + 1): 
+        if eh_primo(j): 
+            lista_primos.append(j)
+    return lista_primos
 
 
-assert lista_primos(10) == [2, 3, 5, 7]
-assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
-assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+def test():
+    assert lista_primos(10) == [2, 3, 5, 7]
+    assert lista_primos(13) == [2, 3, 5, 7, 11, 13]
+    assert lista_primos(50) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+
+
--- 11_Funcoes_e_parametros_avancados/primo.py
+++ 11_Funcoes_e_parametros_avancados/primo.py
@@ -1,11 +1,18 @@
 def eh_primo(numero: int) -> bool:
-    pass
+    if numero <= 1:
+        return False
+    i = 2
+    while numero % i != 0:
+        i = i + 1
+    if i == numero:
+        return True
+    return False
 
-
-assert not eh_primo(-1)
-assert not eh_primo(0)
-assert not eh_primo(1)
-assert eh_primo(2)
-assert eh_primo(3)
-assert not eh_primo(4)
-assert eh_primo(5)
+def test(): 
+    assert not eh_primo(-1)
+    assert not eh_primo(0)
+    assert not eh_primo(1)
+    assert eh_primo(2)
+    assert eh_primo(3)
+    assert not eh_primo(4)
+    assert eh_primo(5)
