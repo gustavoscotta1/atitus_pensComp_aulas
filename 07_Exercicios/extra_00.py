e909243635e3530cfda42037dcddd94dc2e2129f
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-20 19:48:46 -0300

    .

--- 07_Exercicios/extra_00.py
+++ 07_Exercicios/extra_00.py
@@ -1,23 +1,13 @@
 ANO_ATUAL = 2025
 
-
 def saudacao(nome, sobrenome, ano_nascimento):
-
-nome = input('Qual o seu nome?')
-sobrenome = input('Qual o seu sobrenome?')
-ano_nascimento = int(input('Que ano você nasceu?'))
-idade = ANO_ATUAL - ano_nascimento
-print(f'olá, "{nome} {sobrenome}". Bom dia! você possui {idade}!')
-
+    if ano_nascimento <= 0 or ano_nascimento > ANO_ATUAL:
+        return None
+    idade = ANO_ATUAL - ano_nascimento
+    return f"Olá, {nome} {sobrenome}. Bom dia! Você possui {idade} anos!"
 
 def test():
-    assert (
-    saudacao("Matheus", "Jardim", 1991)
-    == "Olá, Matheus Jardim. Bom dia! Você possui 33 anos!"
-    )
-    assert (
-    saudacao("Thais", "Silva", 1990)
-    == "Olá, Thais Silva. Bom dia! Você possui 34 anos!"
-    )
+    assert saudacao("Matheus", "Jardim", 1991) == "Olá, Matheus Jardim. Bom dia! Você possui 34 anos!"
+    assert saudacao("Thais", "Silva", 1990) == "Olá, Thais Silva. Bom dia! Você possui 35 anos!"
     assert saudacao("Matheus", "Jardim", 0) is None
     assert saudacao("Matheus", "Jardim", 2050) is None
