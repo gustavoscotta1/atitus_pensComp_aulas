fe6a8732ca26c028e59463327813dc5f01a18fa7
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-20 20:39:26 -0300

    alter.

--- 10_Introducao_a_funcoes/calculadora_datas.py
+++ 10_Introducao_a_funcoes/calculadora_datas.py
@@ -6,17 +6,16 @@ MESES_31_DIAS = [1, 3, 5, 7, 8, 10, 12]
 MESES_30_DIAS = [4, 6, 9, 11]
 
 def eh_bissexto(ano: int) -> bool:
-    return (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0)
+    return (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0)
 
 def total_dias_no_mes(mes: int, ano: int) -> int:
     if mes in MESES_31_DIAS:
         return 31
     elif mes in MESES_30_DIAS:
         return 30
-    elif mes == 2:
-        return 29 if eh_bissexto(ano) else 28
     else:
-        raise ValueError("Mês inválido")
+        # Fevereiro
+        return 29 if eh_bissexto(ano) else 28
 
 def formata_data(data: list) -> str:
     dia, mes, ano = data
@@ -33,9 +32,10 @@ def calcula_diferenca(data1: list, data2: list) -> int:
         dias += dia
         return dias
 
-    return abs(dias_desde_ano_zero(data1) - dias_desde_ano_zero(data2)) - 1
+    return abs(dias_desde_ano_zero(data1) - dias_desde_ano_zero(data2))
 
-def test(): 
+
+def test():
     assert total_dias_no_mes(1, 2024) == 31
     assert total_dias_no_mes(2, 2024) == 29
     assert total_dias_no_mes(3, 2024) == 31
@@ -45,8 +45,8 @@ def test():
     assert formata_data([1, 12, 2024]) == "1/12/2024"
 
     assert calcula_diferenca([2, 7, 2004], [27, 5, 2024]) == 7268
-    assert calcula_diferenca([27, 5, 2024], [2, 7, 2004 + 85]) == 23779
-    assert calcula_diferenca([2, 7, 2004], [2, 7, 2004 + 85]) == 31047
+    assert calcula_diferenca([27, 5, 2024], [2, 7, 2089]) == 23779
+    assert calcula_diferenca([2, 7, 2004], [2, 7, 2089]) == 31047
     assert calcula_diferenca([24, 7, 1991], [24, 10, 2024]) == 12146
-    assert calcula_diferenca([24, 10, 2024], [24, 7, 1991 + 85]) == 18900
-    assert calcula_diferenca([24, 7, 1991], [24, 7, 1991 + 85]) == 31046
+    assert calcula_diferenca([24, 10, 2024], [24, 7, 2076]) == 18900
+    assert calcula_diferenca([24, 7, 1991], [24, 7, 2076]) == 31046
