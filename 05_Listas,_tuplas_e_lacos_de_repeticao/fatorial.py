10be1f48f9d3a569d03f87b484ed4110c5a74837
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-20 19:27:38 -0300

    alteração

--- 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
+++ 05_Listas,_tuplas_e_lacos_de_repeticao/fatorial.py
@@ -7,13 +7,13 @@ def fatorial(numero):
         resultado *= i
         i += 1
     return resultado
-    
 
 
-assert fatorial(0) == 1
-assert fatorial(1) == 1
-assert fatorial(2) == 2
-assert fatorial(3) == 6
-assert fatorial(4) == 24
-assert fatorial(5) == 120
-assert fatorial(-1) is None
+def test_fatorial(): 
+    assert fatorial(0) == 1
+    assert fatorial(1) == 1
+    assert fatorial(2) == 2
+    assert fatorial(3) == 6
+    assert fatorial(4) == 24
+    assert fatorial(5) == 120
+    assert fatorial(-1) is None
