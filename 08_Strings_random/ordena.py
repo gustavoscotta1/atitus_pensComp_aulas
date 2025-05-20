83b703d1dd56a64b2d00afea66a42b1c156d9d20
gustavoscotta1 <gustavoscotta2006@gmail.com>
2025-05-20 20:12:38 -0300

    ateração

--- 08_Strings_random/ordena.py
+++ 08_Strings_random/ordena.py
@@ -1,13 +1,11 @@
 def ordena_lista(lista):
-    for i in range (len(lista)):
-        for j in range (len(lista)-1):
-            if lista[j] > lista[j+1]
+    for i in range(len(lista)):
+        for j in range(len(lista) - 1):
+            if lista[j] > lista[j + 1]:
                 aux = lista[j]
-                lista[j] = lista[j+1]
-                lista[j+1] = aux
+                lista[j] = lista[j + 1]
+                lista[j + 1] = aux
     return lista
-                
-
 
 def test():
     assert ordena_lista([]) == []
